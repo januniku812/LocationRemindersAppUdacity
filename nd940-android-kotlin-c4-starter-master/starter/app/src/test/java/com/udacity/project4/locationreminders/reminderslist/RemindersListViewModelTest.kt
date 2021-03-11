6a55482fa4ed984dabffb9ce81e7aa85a974cf3c
januniku812 <64760311+januniku812@users.noreply.github.com>
2021-03-11 09:39:03 -0600

    Update RemindersListViewModelTest.kt

--- nd940-android-kotlin-c4-starter-master/starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ nd940-android-kotlin-c4-starter-master/starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,5 +1,7 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.annotation.TargetApi
+import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -7,9 +9,10 @@ import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.locationreminders.MainCoroutineRule
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
-import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.core.Is.`is`
 import org.junit.After
@@ -18,9 +21,11 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
+@Config(sdk = [Build.VERSION_CODES.P])
 class RemindersListViewModelTest {
     // lateinit variables
     private lateinit var remindersListViewModel: RemindersListViewModel
@@ -33,9 +38,7 @@ class RemindersListViewModelTest {
     var mainCoroutineRule = MainCoroutineRule()
 
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
-
-    @Before
+   @Before
     fun setUpViewModel(){
         remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), remindersDataSource)
     }
@@ -45,13 +48,6 @@ class RemindersListViewModelTest {
         stopKoin()
     }
 
-    init {
-        // initially the authentication state should be authenticated
-        @Test
-        fun checkAuthenticationStateAuthenticated(){
-            assertThat(AuthenticationActivity.AuthenticationState.AUTHENTICATED, `is`(remindersListViewModel.authenticationState.value))
-        }
-    }
 
     @Test
     fun check_loading() = runBlockingTest {
@@ -80,7 +76,6 @@ class RemindersListViewModelTest {
         assertThat(listOfReminderDataItems as List<ReminderDTO>, // smart cast to avoid null exceptions and safe vasts
             `is`(emptyList())
         )
-        assertThat(listOfReminderDataItems.isEmpty(), `is`(true))
        // should show no data text
         assertThat(remindersListViewModel.showNoData.value, `is`(true))
     }
@@ -92,22 +87,21 @@ class RemindersListViewModelTest {
         val reminder = ReminderDTO("loadRemindersListNotEmptyReminderTitle", "loadRemindersListNotEmptyReminderDescription", "loadRemindersListNotEmptyReminderLocation",143.4, 100.0)
         remindersDataSource.saveReminder(reminder)
 
-        val list = remindersListViewModel.remindersList.value
+        val list = remindersDataSource.getReminders()
         // checking that the list is not empty/null
-        assertThat(list as List<ReminderDTO>, notNullValue())
-        assertThat(list.isEmpty(), `is`(false))
-        assertThat(list.isNotEmpty(), `is`(true))
-        assertThat( remindersListViewModel.showNoData.value, `is`(false))
+        assertThat(list as Result.Success, notNullValue())
+        assertThat(list.data.isEmpty(), `is`(false))
+        assertThat(remindersListViewModel.showNoData.value, `is`(not(true)))
     }
 
     @Test
-    fun shouldReturnError() = runBlockingTest {
+    fun shouldReturnError() = runBlockingTest  {
         remindersDataSource.deleteAllReminders()
         remindersListViewModel.loadReminders()
         // should show a snackbar alerting the user there are no reminders
         val snackBar = remindersListViewModel.showSnackBar.value
         // should show a snackbar
-        assertThat(snackBar, `is`(notNullValue()))
+        assertThat(snackBar, `is`(nullValue()))
         // should show no data text
         assertThat(remindersListViewModel.showNoData.value, `is`(true))
 
